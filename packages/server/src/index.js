7627325f06c2d83c20a792d9ed71f4678c830b3b
Diego Mais <diegomais.dev@gmail.com>
2020-12-25 16:43:33 -0300

    ✨ (server): connect the SpaceX v2 REST API
    To do so, we use the RESTDataSource class from the apollo-datasource-rest package.
    This class is an extension of DataSource that handles fetching data from a REST API.
    To use this class, we extend it and provide it the base URL of the REST API it will communicate with.

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,7 +1,15 @@
 const { ApolloServer } = require('apollo-server')
+const LaunchAPI = require('./datasources/launch')
+const resolvers = require('./resolvers')
 const typeDefs = require('./schema')
 
-const server = new ApolloServer({ typeDefs })
+const server = new ApolloServer({
+  dataSources: () => ({
+    launchAPI: new LaunchAPI()
+  }),
+  resolvers,
+  typeDefs
+})
 
 const PORT = 4000
 
