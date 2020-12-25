bda08a5f1f9cbbf2c9353729cdf855d158d78d18
Diego Mais <diegomais.dev@gmail.com>
2020-12-25 17:04:03 -0300

    ✨ (server): add custom resolver for Mission.missionPatch
    The resolver for Mission.missionPatch should return a different value depending on whether a query specifies LARGE or SMALL for the size argument.
    This resolver obtains a large or small patch from mission, which is the object returned by the default resolver for the parent field in our schema, Launch.mission.

--- packages/server/src/resolvers.js
+++ packages/server/src/resolvers.js
@@ -1,4 +1,11 @@
 module.exports = {
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge
+    }
+  },
   Query: {
     launches: (_, __, { dataSources }) =>
       dataSources.launchAPI.getAllLaunches(),
